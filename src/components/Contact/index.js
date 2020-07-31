8d019d0c26041aba485a253480af8885ac35bcc8
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-07-30 20:52:46 -0500

    finalize contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -19,13 +19,12 @@ function ContactForm() {
             }
         }
         else {
-            if(!e.target.length) {
-                setErrorMessage(`${e.target.value.name} is required.`);
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
             }
-            else {
-                setErrorMessage('');
-            }
-        }
+          }
 
         if (!errorMessage) {
             setFormState({...formState, [e.target.name]: e.target.value })
@@ -43,18 +42,26 @@ function ContactForm() {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
+                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5"  />
 
                 </div>
+            
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+            
                 <button type="submit">Submit</button>
+
             </form>
         </section>
     );
