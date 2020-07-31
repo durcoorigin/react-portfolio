d284684b174aa9e7fa6e170181bec515ee97e986
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-07-30 20:10:43 -0500

    add contact form validation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Portfolio></Portfolio>
         <About></About>
       </main>
