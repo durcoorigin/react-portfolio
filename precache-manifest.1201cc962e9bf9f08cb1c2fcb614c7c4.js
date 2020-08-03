43a057903db61ada3c403847c5f8674301b5403a
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-08-03 14:27:57 -0500

    Updates

---
+++ precache-manifest.1201cc962e9bf9f08cb1c2fcb614c7c4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dded9eafd3ee5d4149fd84cf6f7af3",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "fd828c769470d4abb158",
+    "url": "/react-portfolio/static/css/main.f80ec1f7.chunk.css"
+  },
+  {
+    "revision": "05d9d68e12d4e2c7fe78",
+    "url": "/react-portfolio/static/js/2.f11bc962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.f11bc962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd828c769470d4abb158",
+    "url": "/react-portfolio/static/js/main.037b981d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "e0e339446da03c533f47120da2a1e549",
+    "url": "/react-portfolio/static/media/Chris-Short.e0e33944.jpg"
+  },
+  {
+    "revision": "27299ba93c7445245c21a048646922cd",
+    "url": "/react-portfolio/static/media/nutrifinder.27299ba9.jpg"
+  },
+  {
+    "revision": "2eca81158b7d9a0e315df3ba2922381e",
+    "url": "/react-portfolio/static/media/photo-port.2eca8115.jpg"
+  },
+  {
+    "revision": "7fc913320cefbfb4350f046edd71505f",
+    "url": "/react-portfolio/static/media/run-buddy.7fc91332.jpg"
+  },
+  {
+    "revision": "6a3b71abc29e5945b29cd5aa22342548",
+    "url": "/react-portfolio/static/media/weather-dashboard.6a3b71ab.jpg"
+  },
+  {
+    "revision": "eee93c66c940eb9c6af9da1edc777d6f",
+    "url": "/react-portfolio/static/media/whiskey-inventory.eee93c66.jpg"
+  },
+  {
+    "revision": "bc48c6574ad61da6feb02deab965fdd7",
+    "url": "/react-portfolio/static/media/wood-pattern.bc48c657.jpg"
+  },
+  {
+    "revision": "89f09600c34ddb969f0bb494098cc62b",
+    "url": "/react-portfolio/static/media/zoo-keeper.89f09600.jpg"
+  }
+]);
